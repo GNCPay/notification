6acabfff5d9f77908561b54255dd59f3c3c9fbf2
huypayflow <huynq@payflow.vn>
2015-08-18 22:08:13 +0700

    Init

---
+++ Payflow.Notification/Payflow.Notification.API/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Payflow.Notification.API
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
