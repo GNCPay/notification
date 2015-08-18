71229cdde842bba435878b90304c1ffba46fc81e
huypayflow <huynq@payflow.vn>
2015-08-18 22:54:19 +0700

    Done SMS

---
+++ Payflow.Notification/Payflow.Notification.API/Controllers/smsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Payflow.Notification.API.Controllers
+{
+    public class SMSController : ApiController
+    {
+        public HttpResponseMessage Put(string user_id, string user_pwd, string sent_to, string sent_content)
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+            bool result = SMS.Send(sent_to, sent_content);
+            if (result) response.StatusCode = HttpStatusCode.OK;
+            else response.StatusCode = HttpStatusCode.BadGateway;
+            return response;
+        }
+    }
+}
--- Payflow.Notification/Payflow.Notification/SMS.cs
+++ Payflow.Notification/Payflow.Notification/SMS.cs
@@ -8,5 +8,49 @@ namespace Payflow.Notification
 {
     public class SMS
     {
+        public static bool Send(
+            string ReceiverTel,
+            string Content)
+        {
+            ReceiverTel = ReceiverTel.Replace(" ", "").Replace("+", "");
+            if (ReceiverTel.StartsWith("0")) ReceiverTel = ReceiverTel.Substring(1, ReceiverTel.Length - 1);
+            if (ReceiverTel.StartsWith("84")) ReceiverTel = ReceiverTel.Remove(0, 2);
+
+            ReceiverTel = "84" + ReceiverTel;
+            string iResult = "0";
+
+            iResult = SendSMSIncom(ReceiverTel, Content, false);
+            //dynamic log = null;// = new Data.Log();
+            //log.Module = "NOTIFICATION";
+            //log.Category = "INFO";
+            //log.LogKey = "SMS";
+            //log.CreatedBy = "notification@epayment.vn";
+            //log.CreatedTime = DateTime.Now;
+            //log.LogMessage = string.Concat("Send SMS (INCOM) to ", ReceiverTel, " return: ", iResult);
+            return iResult == "1";
+        }
+
+        public static string SendSMSIncom(string ReceiverTel, string Content, bool isBrandName)
+        {
+            string account_name = "Payflowdauso";
+            string account_pwd = "AccOUnSenDMTdauso6x8938mtSS";
+            string brand_code = "PAYID";
+            string brand_num = "6089";
+            string command_code = "PAYID";
+            sms.incom.Service_SendSMSSoapClient proxy = new sms.incom.Service_SendSMSSoapClient();
+            return proxy.SendSMS(
+                account_name
+                , account_pwd
+                , ReceiverTel,
+                Content,
+                (isBrandName) ? brand_code : brand_num,//NganhangMDB
+                command_code,
+                "0",
+                "0",
+                "1",
+                "1",
+                "0",
+                "0").ToString();
+        }
     }
 }
